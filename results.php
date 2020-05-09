2558aa3fa4bee86dc6338d69b7280f6707ae9a57
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-05-08 22:32:58 -0700

    nav bar file

--- results.php
+++ results.php
@@ -50,44 +50,7 @@
 <body>
 
 	<div class="container-fluid">
-		<!-- header -->
-		<div class="row header">
-
-			<!-- nav bar -->
-			<div class="col col-3 center">
-				<nav class="navbar navbar-expand-lg navbar-light">
-				  <button class="navbar-toggler button-color" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				    <span class="navbar-toggler-icon"></span>
-				  </button>
-				  <div class="collapse navbar-collapse" id="navbarNav">
-				    <ul class="navbar-nav">
-				      <li class="nav-item">
-				        <a class="nav-link" href="index.html"><span class="item-color">Home</span></a>
-				      </li>
-				      <li class="nav-item">
-				        <a class="nav-link" href="search.php"><span class="item-color">Search</span></a>
-				      </li>
-				      <li class="nav-item">
-				        <a class="nav-link" href="browse.php"><span class="item-color">Browse</span></a>
-				      </li>
-				    </ul>
-				  </div>
-				</nav>
-			</div>
-
-			<!-- title -->
-			<div class="col col-6 center" id="title">
-				<a href="index.html">
-					<h1><span>USC </span>Rate My Dorm</h1>
-				</a>
-			</div>
-
-			<!-- icon -->
-			<div class="col col-3" id="icon">
-				<img src="images/icon.png" alt="USC icon"/>
-			</div>
-
-		</div>
+		<?php include "nav.html"; ?>
 
 		<div class="row row-margin">
 			<div class="col col-12 heading">
