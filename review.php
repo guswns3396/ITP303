4bab48a10cf3f0c2b7b8289021e0b71d7941855f
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-05-09 20:20:17 -0700

    update function

--- review.php
+++ review.php
@@ -58,6 +58,10 @@
 
 		<form action="review_confirmation.php" method="POST">
 			<input type="hidden" name="isUpdate" value="<?php echo $isUpdate; ?>"/>
+			<?php if ($isUpdate) : ?>
+				<input type="hidden" name="review_id" value="<?php echo $_POST["review_id"]; ?>"/>
+			<?php endif; ?>
+			<input type="hidden" name="isUpdate" value="<?php echo $isUpdate; ?>"/>
 			<div class="form-group row">
 				<label for="name-id" class="col-sm-3 col-form-label text-sm-right">Name:</label>
 				<div class="col-sm-6">
